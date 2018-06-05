601da000271d34631c4a5b18f8e92b7782757bbc
Sandun <sandunwebdev@gmail.com>
2018-06-05 21:49:56 +0530

    Finished Contac Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import Header from "../Header/Header"; // Header Component
 import Intro from "../Intro/Intro"; // Intro Page
 import Skills from "../Skills/Skills"; // Skills Page
 import Projects from "../Projects/Projects"; // Projects Page
+import Contact from "../Contact/Contact"; // Contact Page
 
 import "normalize.css"; // Using Famous Noramlize.CSS to Reset CSS
 import "./App.css";
@@ -16,11 +17,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <h1>Welcome To Sandun's Portfolio</h1> */}
         <Header />
         <Intro />
         <Skills />
         <Projects />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,34 @@
+/**
+ * Contact Component - Render Contact Details
+ */
+
+import React, { Component } from "react";
+
+// Font Awesome Resources
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faGoogle from "@fortawesome/fontawesome-free-brands/faGoogle";
+import faLinkedin from "@fortawesome/fontawesome-free-brands/faLinkedin";
+import faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
+import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
+
+
+import "./Contact.css";
+
+class Skills extends Component {
+  render() {
+    return (
+      <div className="Contact" id="Contact">
+       <h1 className="section__header Contact__header">CONTACT</h1>
+       <div className="Contact__cards__wrapper">
+       <h1>If you wanna get in touch, talk to me about a project or just say hi, Feel free to contact me through follwing mediums. I look forward to hearing from you.</h1>
+       <a href="mailto:SandunWebDev@gmail.com" target="_blank"><FontAwesomeIcon icon={faGoogle} className="faIcon" /></a>
+       <a href="https://github.com/SandunWebDev" target="_blank"><FontAwesomeIcon icon={faGithub} className="faIcon" /></a>
+       <a href="https://www.linkedin.com/in/SandunWebDev" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="faIcon" /></a>
+       <a href="https://twitter.com/SandunWebDev" target="_blank"><FontAwesomeIcon icon={faTwitter} className="faIcon" /></a>
+       </div>
+        </div>
+    );
+  }
+}
+
+export default Skills;
