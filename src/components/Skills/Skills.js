cf4ef95d27f6ea6be7e6458d8d06619bb1dbebc7
Sandun <SandunWebDev@gmail.com>
2020-10-14 17:19:01 +0530

    Updated skills list.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -34,18 +34,20 @@ class Skills extends Component {
 							<SkillCard logo={skillLogos.css} title="CSS3" />
 							<SkillCard logo={skillLogos.sass} title="SAAS" />
 							<SkillCard logo={skillLogos.bootstrap} title="BOOTSTRAP" />
-							<SkillCard logo={skillLogos.d3} title="D3" />
+
+							<SkillCard logo={skillLogos.js} title="JAVASCRIPT" />
+							<SkillCard logo={skillLogos.jquery} title="JQUERY" />
+							<SkillCard
+								logo={skillLogos.react}
+								s
+								title="REACT / R.NATIVE / REDUX"
+							/>
+							<SkillCard logo={skillLogos.angular} title="ANGULAR" />
+							<SkillCard logo={skillLogos.d3} title="D3 / CHARTS" />
 							<SkillCard
 								logo={skillLogos.responsive}
 								title="RESPONSIVE DESIGN"
 							/>
-							<SkillCard logo={skillLogos.js} title="JS ES6" />
-							<SkillCard logo={skillLogos.jquery} title="JQUERY" />
-							<SkillCard
-								logo={skillLogos.react}
-								title="REACT / NATIVE / REDUX"
-							/>
-							<SkillCard logo={skillLogos.angular} title="ANGULAR" />
 						</div>
 					</div>
 					<div className="Skills__category">
@@ -55,12 +57,41 @@ class Skills extends Component {
 							<SkillCard logo={skillLogos.express} title="EXPRESS" />
 							<SkillCard logo={skillLogos.mongodb} title="MONGODB" />
 							<SkillCard logo={skillLogos.mongoose} title="MONGOOSE" />
-							<SkillCard logo={skillLogos.firebase} title="FIREBASE" />
-							<SkillCard logo={skillLogos.mysql} title="MYSQL / SQLITE" />
+							<SkillCard
+								logo={skillLogos.mysql}
+								title="MYSQL / POSTGRES / SQLITE"
+							/>
+							<SkillCard logo={skillLogos.sequelize} title="SEQUELIZE" />
 							<SkillCard logo={skillLogos.auth} title="AUTHENTICATION" />
 							<SkillCard logo={skillLogos.passport} title="REST API" />
+							<SkillCard logo={skillLogos.firebase} title="FIREBASE" />
 							<SkillCard logo={skillLogos.php} title="PHP" />
 							<SkillCard logo={skillLogos.python} title="PYTHON" />
+						</div>
+					</div>
+					<div className="Skills__category">
+						<div className="category__header">DEVOPS / TOOLS</div>
+						<div className="category__content">
+							<SkillCard logo={skillLogos.cicd} title="CI/CD" />
+							<SkillCard logo={skillLogos.docker} title="DOCKER / D. COMPOSE" />
+							<SkillCard logo={skillLogos.kubernetes} title="KUBERNETES" />
+							<SkillCard
+								logo={skillLogos.gcloud}
+								title="GCLOUD / AWS / HEROKU "
+							/>
+							<SkillCard
+								logo={skillLogos.jest}
+								title="JEST / MOCHA / CYPRESS / SINON"
+							/>
+							<SkillCard logo={skillLogos.storybook} title="STORYBOOK" />
+							<SkillCard logo={skillLogos.git} title="GIT & GITHUB" />
+							<SkillCard logo={skillLogos.npm} title="NPM & YARN" />
+							<SkillCard
+								logo={skillLogos.webpack}
+								title="WEBPACK / GRUNT / BABEL"
+							/>
+							<SkillCard logo={skillLogos.typescript} title="TYPESCRIPT" />
+							<SkillCard logo={skillLogos.terminal} title="COMMAND LINE" />
 						</div>
 					</div>
 					<div className="Skills__category">
@@ -75,30 +106,15 @@ class Skills extends Component {
 						</div>
 					</div>
 					<div className="Skills__category">
-						<div className="category__header">TOOLS</div>
-						<div className="category__content">
-							<SkillCard logo={skillLogos.git} title="GIT & GITHUB" />
-							<SkillCard logo={skillLogos.npm} title="NPM & YARN" />
-							<SkillCard logo={skillLogos.webpack} title="WEBPACK" />
-							<SkillCard logo={skillLogos.grunt} title="GRUNT" />
-							<SkillCard logo={skillLogos.babel} title="BABEL" />
-							<SkillCard logo={skillLogos.mocha} title="MOCHA / CHAI / JEST" />
-							<SkillCard logo={skillLogos.typescript} title="TYPESCRIPT" />
-							<SkillCard logo={skillLogos.terminal} title="COMMAND LINE" />
-						</div>
-					</div>
-					<div className="Skills__category">
 						<div className="category__header">OTHER</div>
 						<div className="category__content">
-							<SkillCard
-								logo={skillLogos.iot}
-								title="IOT / ARDUINO / RASPBERRY PI / ESP"
-							/>
+							<SkillCard logo={skillLogos.iot} title="IOT / ARDUINO / ESP" />
 							<SkillCard logo={skillLogos.c} title="C LANG." />
 							<SkillCard logo={skillLogos.agile} title="AGILE / SCRUM" />
 							<SkillCard logo={skillLogos.linux} title="LINUX" />
-							<SkillCard logo={skillLogos.heroku} title="HEROKU / AMAZON" />
 							<SkillCard logo={skillLogos.electron} title="ELECTRON" />
+							<SkillCard logo={skillLogos.postman} title="POSTMAN" />
+							<SkillCard logo={skillLogos.slack} title="SLACK / TRELLO / ..." />
 						</div>
 					</div>
 				</div>
